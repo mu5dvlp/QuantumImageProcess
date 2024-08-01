c95a036993131d1677828ba3eea090c644050ac0
mu5dvlp <mu5dvlp@gmail.com>
2024-08-01 18:14:45 +0900

    リファクタ

--- common_quantum.py
+++ common_quantum.py
@@ -3,7 +3,8 @@ from qiskit import *
 
 # //＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 def create_QuantumCircuit_by_Statevector(sv):
-    n_qubits = len(sv).bit_length()
+    length = len(sv)-1
+    n_qubits = length.bit_length()
     qc = QuantumCircuit(n_qubits,n_qubits)
     qc.initialize(sv,range(n_qubits))
     qc.measure(range(n_qubits),range(n_qubits))
