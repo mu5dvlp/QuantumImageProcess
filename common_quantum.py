3a4d44f5dc94e965c6af7b4b416dd44d2be5b1dc
mu5dvlp <mu5dvlp@gmail.com>
2024-08-01 18:27:23 +0900

    リファクタ

--- common_quantum.py
+++ common_quantum.py
@@ -9,3 +9,14 @@ def create_QuantumCircuit_by_Statevector(sv):
     qc.initialize(sv,range(n_qubits))
     qc.measure(range(n_qubits),range(n_qubits))
     return qc
+
+def convert_probability2statevector(probabilities,address):
+    width = len(probabilities[0])
+    height = len(probabilities)
+    n_qubits = width.bit_length() + height.bit_length()
+    statevector = np.zeros(2**n_qubits)
+    for y in range(height):
+        for x in range(width):
+            index = int(address[y][x],2)
+            statevector[index] = probabilities[y][x]
+    return statevector
