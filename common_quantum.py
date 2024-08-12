46f17b406c96983a92857e3af5e14541243c58f4
mu5dvlp <mu5dvlp@gmail.com>
2024-08-12 13:44:22 +0900

    量子回路 修正

--- common_quantum.py
+++ common_quantum.py
@@ -2,18 +2,22 @@ import numpy as np
 from qiskit import *
 
 # //＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
+def get_qubit_counts(length):
+    return (length-1).bit_length()
+
+
 def create_QuantumCircuit_by_Statevector(sv):
-    length = len(sv)-1
-    n_qubits = length.bit_length()
+    n_qubits = int(np.log2(len(sv)))
     qc = QuantumCircuit(n_qubits,n_qubits)
     qc.initialize(sv,range(n_qubits))
     qc.measure(range(n_qubits),range(n_qubits))
     return qc
 
+
 def convert_probability2statevector(probabilities,address):
     width = len(probabilities[0])
     height = len(probabilities)
-    n_qubits = width.bit_length() + height.bit_length()
+    n_qubits = get_qubit_counts(width) + get_qubit_counts(height)
     statevector = np.zeros(2**n_qubits)
     for y in range(height):
         for x in range(width):
